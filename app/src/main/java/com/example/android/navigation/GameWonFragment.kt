8dc0573b40cf6c1e555385904b51112b33977d2c
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-06-02 18:30:22 -0300

    add safeargs

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,10 +20,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
+import kotlinx.android.synthetic.main.fragment_game_won.*
 
 
 class GameWonFragment : Fragment() {
@@ -34,8 +36,13 @@ class GameWonFragment : Fragment() {
                 inflater, R.layout.fragment_game_won, container, false)
 
         binding.nextMatchButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
+
+        var args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context,
+            "Nº Correct: ${args.numCorrect}, Nº questions: ${args.numQuestions}",
+            Toast.LENGTH_LONG).show()
 
         return binding.root
     }
